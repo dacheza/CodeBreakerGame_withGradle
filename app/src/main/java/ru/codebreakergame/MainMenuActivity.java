bd11f30dd682b051e78646252e7df775707e669a
Citlalatonac <sainthellmymind@gmail.com>
2016-01-20 11:42:29 +0200

    Added button in main menu (continue and new game)

--- app/src/main/java/ru/codebreakergame/MainMenuActivity.java
+++ app/src/main/java/ru/codebreakergame/MainMenuActivity.java
@@ -4,12 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-public class MainActivity extends Activity {
+public class MainMenuActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_mainmenu);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
