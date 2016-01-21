f7eab8b0aa4c5b6eb97dd8da67ee68b38a56252d
Citlalatonac <sainthellmymind@gmail.com>
2016-01-21 10:12:03 +0200

    ScreenOrientation only portrait, added fullscreenmod and keepOnScreen

--- app/src/main/java/ru/codebreakergame/GameActivity.java
+++ app/src/main/java/ru/codebreakergame/GameActivity.java
@@ -2,7 +2,6 @@ package ru.codebreakergame;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.WindowManager;
 
 public class GameActivity extends Activity {
 
@@ -10,9 +9,5 @@ public class GameActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
-        /** Скрытие статус бара */
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
     }
 }
--- app/src/main/java/ru/codebreakergame/MainMenuActivity.java
+++ app/src/main/java/ru/codebreakergame/MainMenuActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.ImageView;
 
 
@@ -14,9 +13,6 @@ public class MainMenuActivity extends Activity implements View.OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmenu);
-
-        /** Скрытие статус бара */
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         ImageView ivContinue = (ImageView) findViewById(R.id.imageViewContinue);
         ImageView ivNewGame = (ImageView) findViewById(R.id.imageViewNewGame);
