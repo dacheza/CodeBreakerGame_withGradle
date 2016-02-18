8242a6aca02cc857846be796da688d93ce3548b7
Citlalatonac <sainthellmymind@gmail.com>
2016-02-18 11:45:11 +0200

    Change gameplay in GameActivity

--- app/src/main/java/ru/codebreakergame/activity/GameActivity.java
+++ app/src/main/java/ru/codebreakergame/activity/GameActivity.java
@@ -3,318 +3,136 @@ package ru.codebreakergame.activity;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 import ru.codebreakergame.R;
+import ru.codebreakergame.chipers.replacementshift.CaesarCode;
 
-/**
- * Еще одна версия игрового класса, все так же кривокода
- */
-public class GameActivity extends Activity implements View.OnClickListener {
+public class GameActivity extends Activity implements View.OnClickListener, View.OnKeyListener {
 
-    TextView textPrev;
-    TextView textNext;
-    TextView chiperTextView;
+    EditText beforeReplace;
+    EditText afterReplace;
     String originalText;
-    String chiperText;
-    LinkedList<String> memoryList = new LinkedList<>();
+    String cipherText;
+    TextView cipherTextView;
+    ArrayList<String> memoryList = new ArrayList<>();
 
-    Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10,
-            btn11, btn12, btn13, btn14, btn15, btn16, btn17, btn18, btn19, btn20,
-            btn21, btn22, btn23, btn24, btn25, btn26, btn27, btn28, btn29, btn30, btn31, btn32,
-            undoText, clearText, replaceText;
+    Button undoTextButton, fixTextButton, replaceTextButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        /*CaesarCode gameOfCaesar = new CaesarCode();*/
-        findId();
-        setClick();
+        CaesarCode gameOfCaesar = new CaesarCode();
+
+        beforeReplace = (EditText) findViewById(R.id.editBeforeReplace);
+        afterReplace = (EditText) findViewById(R.id.editAfterReplace);
+
+        beforeReplace.setOnKeyListener(this);
+        afterReplace.setOnKeyListener(this);
+
+        cipherTextView = (TextView) findViewById(R.id.cipherTextView);
 
         originalText = getString(R.string.originalTextLevelOne);
-        chiperText = /*gameOfCaesar.codeCaesar(originalText, 2)*/  originalText.toLowerCase();
-        chiperTextView.setText(chiperText);
-        chiperTextView.setTextColor(Color.BLACK);
+        cipherText = gameOfCaesar.codeMaker(originalText, 2);
 
-        memoryList.add(chiperText);
+        cipherTextView.setText(cipherText);
+        cipherTextView.setTextColor(Color.BLACK);
 
-        replaceText.setClickable(false);
-        clearText.setClickable(false);
-        undoText.setClickable(false);
+        replaceTextButton = (Button) findViewById(R.id.replaceButton);
+        fixTextButton = (Button) findViewById(R.id.fixButton);
+        undoTextButton = (Button) findViewById(R.id.undoButton);
+
+        replaceTextButton.setOnClickListener(this);
+        fixTextButton.setOnClickListener(this);
+        undoTextButton.setOnClickListener(this);
+
+
+        memoryList.add(cipherText);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.buttonAlphabetWord_1:
-                setTextOnWordReplaceWindow(btn1);
+            case R.id.replaceButton:
+                replaceText();
                 break;
-            case R.id.buttonAlphabetWord_2:
-                setTextOnWordReplaceWindow(btn2);
+            case R.id.fixButton:
+                fixText();
                 break;
-            case R.id.buttonAlphabetWord_3:
-                setTextOnWordReplaceWindow(btn3);
-                break;
-            case R.id.buttonAlphabetWord_4:
-                setTextOnWordReplaceWindow(btn4);
-                break;
-            case R.id.buttonAlphabetWord_5:
-                setTextOnWordReplaceWindow(btn5);
-                break;
-            case R.id.buttonAlphabetWord_6:
-                setTextOnWordReplaceWindow(btn6);
-                break;
-            case R.id.buttonAlphabetWord_7:
-                setTextOnWordReplaceWindow(btn7);
-                break;
-            case R.id.buttonAlphabetWord_8:
-                setTextOnWordReplaceWindow(btn8);
-                break;
-            case R.id.buttonAlphabetWord_9:
-                setTextOnWordReplaceWindow(btn9);
-                break;
-            case R.id.buttonAlphabetWord_10:
-                setTextOnWordReplaceWindow(btn10);
-                break;
-            case R.id.buttonAlphabetWord_11:
-                setTextOnWordReplaceWindow(btn11);
-                break;
-            case R.id.buttonAlphabetWord_12:
-                setTextOnWordReplaceWindow(btn12);
-                break;
-            case R.id.buttonAlphabetWord_13:
-                setTextOnWordReplaceWindow(btn13);
-                break;
-            case R.id.buttonAlphabetWord_14:
-                setTextOnWordReplaceWindow(btn14);
-                break;
-            case R.id.buttonAlphabetWord_15:
-                setTextOnWordReplaceWindow(btn15);
-                break;
-            case R.id.buttonAlphabetWord_16:
-                setTextOnWordReplaceWindow(btn16);
-                break;
-            case R.id.buttonAlphabetWord_17:
-                setTextOnWordReplaceWindow(btn17);
-                break;
-            case R.id.buttonAlphabetWord_18:
-                setTextOnWordReplaceWindow(btn18);
-                break;
-            case R.id.buttonAlphabetWord_19:
-                setTextOnWordReplaceWindow(btn19);
-                break;
-            case R.id.buttonAlphabetWord_20:
-                setTextOnWordReplaceWindow(btn20);
-                break;
-            case R.id.buttonAlphabetWord_21:
-                setTextOnWordReplaceWindow(btn21);
-                break;
-            case R.id.buttonAlphabetWord_22:
-                setTextOnWordReplaceWindow(btn22);
-                break;
-            case R.id.buttonAlphabetWord_23:
-                setTextOnWordReplaceWindow(btn23);
-                break;
-            case R.id.buttonAlphabetWord_24:
-                setTextOnWordReplaceWindow(btn24);
-                break;
-            case R.id.buttonAlphabetWord_25:
-                setTextOnWordReplaceWindow(btn25);
-                break;
-            case R.id.buttonAlphabetWord_26:
-                setTextOnWordReplaceWindow(btn26);
-                break;
-            case R.id.buttonAlphabetWord_27:
-                setTextOnWordReplaceWindow(btn27);
-                break;
-            case R.id.buttonAlphabetWord_28:
-                setTextOnWordReplaceWindow(btn28);
-                break;
-            case R.id.buttonAlphabetWord_29:
-                setTextOnWordReplaceWindow(btn29);
-                break;
-            case R.id.buttonAlphabetWord_30:
-                setTextOnWordReplaceWindow(btn30);
-                break;
-            case R.id.buttonAlphabetWord_31:
-                setTextOnWordReplaceWindow(btn30);
-                break;
-            case R.id.buttonAlphabetWord_32:
-                setTextOnWordReplaceWindow(btn30);
-                break;
-            case R.id.buttonCleanText:
-                cleanTextInWordReplaceWindow();
-                break;
-            case R.id.buttonOkReplace:
-                replacerText();
-                undoText.setClickable(true);
-                break;
-            case R.id.buttonUndo:
-                undo();
+            case R.id.undoButton:
+                undoText();
                 break;
         }
     }
 
-    /**
-     * Вставка текста в окошко с буквами
-     */
-    public void setTextOnWordReplaceWindow(Button button) {
+    public void replaceText() {
+        if (beforeReplace.getText().length() > 0) {
+            if (afterReplace.getText().length() > 0) {
 
-        if (textPrev.getText().length() < 1) {
+                String bR = String.valueOf(beforeReplace.getText());
+                String aR = String.valueOf(afterReplace.getText());
+                String text = String.valueOf(cipherTextView.getText());
 
-            textPrev.setText(button.getText());
-            textPrev.setTextColor(Color.BLACK);
+                text = text.replace(bR.toLowerCase(), aR.toUpperCase());
+                cipherTextView.setText(text);
+                memory(text);
 
-            clearText.setClickable(true);
-
-        } else {
-
-            textNext.setText(button.getText());
-            textNext.setTextColor(Color.BLACK);
-
-            replaceText.setClickable(true);
+                beforeReplace.setText("");
+                afterReplace.setText("");
+            }
         }
     }
 
-    /**
-     * Очистка текста из окошка с буквами
-     */
-    public void cleanTextInWordReplaceWindow() {
-        textNext.setText("");
-        textPrev.setText("");
+    public void fixText() {
+        if (beforeReplace.getText().length() > 0) {
+            if (afterReplace.getText().length() > 0) {
+                String bR = String.valueOf(beforeReplace.getText());
+                String aR = String.valueOf(afterReplace.getText());
+                String text = String.valueOf(cipherTextView.getText());
 
-        replaceText.setClickable(false);
-        clearText.setClickable(false);
-    }
+                text = text.replace(bR.toUpperCase(), aR.toUpperCase());
+                cipherTextView.setText(text);
+                memory(text);
 
-    /**
-     * Память для возврата к предыдущему варианту
-     */
-    public void memory(String text) {
-        if (memoryList.size() == 19) {
-            memoryList.remove(0);
-            memoryList.add(text);
-        } else {
-            memoryList.add(text);
+                beforeReplace.setText("");
+                afterReplace.setText("");
+            }
         }
     }
 
-    /**
-     * Возврат к предыдущему варианту
-     */
-    public void undo() {
+    public void undoText() {
         if (memoryList.size() > 1) {
             memoryList.remove(memoryList.size() - 1);
-            chiperText = memoryList.get(memoryList.size() - 1);
-            if (memoryList.size() == 1)
-                undoText.setClickable(false);
-            else
-                undoText.setClickable(true);
-            chiperTextView.setText(chiperText);
+            cipherTextView.setText(memoryList.get(memoryList.size() - 1));
         }
     }
 
-    /**
-     * Замена букв в шифр тексте для наблюдения изменений и дальнейшего прогресса
-     */
-    public void replacerText() {
-        chiperText = chiperTextView.getText().toString();
-        String before = textPrev.getText().toString().toLowerCase();
-        String after = textNext.getText().toString().toLowerCase();
-        before = before.substring(2, 3);
-        after = after.substring(2, 3);
-        chiperText = chiperText.replace(before, after);
-        memory(chiperText);
-        chiperTextView.setText(chiperText);
-
-        cleanTextInWordReplaceWindow();
+    public void memory(String text) {
+        if (memoryList.size() == 20) {
+            memoryList.remove(0);
+            memoryList.add(text);
+            memoryList.trimToSize();
+        } else {
+            memoryList.add(text);
+        }
     }
 
-    public void findId() {
-        textPrev = (TextView) findViewById(R.id.textViewPrevious);
-        textNext = (TextView) findViewById(R.id.textViewNext);
-        chiperTextView = (TextView) findViewById(R.id.textViewChiper);
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN &&
+                (keyCode == KeyEvent.KEYCODE_ENTER)) {
+            replaceText();
+            return true;
+        }
 
-        btn1 = (Button) findViewById(R.id.buttonAlphabetWord_1);
-        btn2 = (Button) findViewById(R.id.buttonAlphabetWord_2);
-        btn3 = (Button) findViewById(R.id.buttonAlphabetWord_3);
-        btn4 = (Button) findViewById(R.id.buttonAlphabetWord_4);
-        btn5 = (Button) findViewById(R.id.buttonAlphabetWord_5);
-        btn6 = (Button) findViewById(R.id.buttonAlphabetWord_6);
-        btn7 = (Button) findViewById(R.id.buttonAlphabetWord_7);
-        btn8 = (Button) findViewById(R.id.buttonAlphabetWord_8);
-        btn9 = (Button) findViewById(R.id.buttonAlphabetWord_9);
-        btn10 = (Button) findViewById(R.id.buttonAlphabetWord_10);
-        btn11 = (Button) findViewById(R.id.buttonAlphabetWord_11);
-        btn12 = (Button) findViewById(R.id.buttonAlphabetWord_12);
-        btn13 = (Button) findViewById(R.id.buttonAlphabetWord_13);
-        btn14 = (Button) findViewById(R.id.buttonAlphabetWord_14);
-        btn15 = (Button) findViewById(R.id.buttonAlphabetWord_15);
-        btn16 = (Button) findViewById(R.id.buttonAlphabetWord_16);
-        btn17 = (Button) findViewById(R.id.buttonAlphabetWord_17);
-        btn18 = (Button) findViewById(R.id.buttonAlphabetWord_18);
-        btn19 = (Button) findViewById(R.id.buttonAlphabetWord_19);
-        btn20 = (Button) findViewById(R.id.buttonAlphabetWord_20);
-        btn21 = (Button) findViewById(R.id.buttonAlphabetWord_21);
-        btn22 = (Button) findViewById(R.id.buttonAlphabetWord_22);
-        btn23 = (Button) findViewById(R.id.buttonAlphabetWord_23);
-        btn24 = (Button) findViewById(R.id.buttonAlphabetWord_24);
-        btn25 = (Button) findViewById(R.id.buttonAlphabetWord_25);
-        btn26 = (Button) findViewById(R.id.buttonAlphabetWord_26);
-        btn27 = (Button) findViewById(R.id.buttonAlphabetWord_27);
-        btn28 = (Button) findViewById(R.id.buttonAlphabetWord_28);
-        btn29 = (Button) findViewById(R.id.buttonAlphabetWord_29);
-        btn30 = (Button) findViewById(R.id.buttonAlphabetWord_30);
-        btn31 = (Button) findViewById(R.id.buttonAlphabetWord_31);
-        btn32 = (Button) findViewById(R.id.buttonAlphabetWord_32);
-
-        undoText = (Button) findViewById(R.id.buttonUndo);
-        clearText = (Button) findViewById(R.id.buttonCleanText);
-        replaceText = (Button) findViewById(R.id.buttonOkReplace);
-    }
-
-    public void setClick() {
-        btn1.setOnClickListener(this);
-        btn2.setOnClickListener(this);
-        btn3.setOnClickListener(this);
-        btn4.setOnClickListener(this);
-        btn5.setOnClickListener(this);
-        btn6.setOnClickListener(this);
-        btn7.setOnClickListener(this);
-        btn8.setOnClickListener(this);
-        btn9.setOnClickListener(this);
-        btn10.setOnClickListener(this);
-        btn11.setOnClickListener(this);
-        btn12.setOnClickListener(this);
-        btn13.setOnClickListener(this);
-        btn14.setOnClickListener(this);
-        btn15.setOnClickListener(this);
-        btn16.setOnClickListener(this);
-        btn17.setOnClickListener(this);
-        btn18.setOnClickListener(this);
-        btn19.setOnClickListener(this);
-        btn20.setOnClickListener(this);
-        btn21.setOnClickListener(this);
-        btn22.setOnClickListener(this);
-        btn23.setOnClickListener(this);
-        btn24.setOnClickListener(this);
-        btn25.setOnClickListener(this);
-        btn26.setOnClickListener(this);
-        btn27.setOnClickListener(this);
-        btn28.setOnClickListener(this);
-        btn29.setOnClickListener(this);
-        btn30.setOnClickListener(this);
-        btn31.setOnClickListener(this);
-        btn32.setOnClickListener(this);
-
-        undoText.setOnClickListener(this);
-        clearText.setOnClickListener(this);
-        replaceText.setOnClickListener(this);
+        return false;
     }
 }
