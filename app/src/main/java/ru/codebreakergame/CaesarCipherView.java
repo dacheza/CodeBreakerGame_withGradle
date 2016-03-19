49c5bdd3767c731a3569b61c4056b0b32d1476e8
dacheza <dacheza@mail.ru>
2016-03-19 04:20:58 +0300

    Create caesar_main_table_layout.xml, caesar_cipher_letter_down.xml, class CaesarCipherView

---
+++ app/src/main/java/ru/codebreakergame/CaesarCipherView.java
@@ -0,0 +1,7 @@
+package ru.codebreakergame;
+
+/**
+ * Created by ee on 19.03.2016.
+ */
+public class CaesarCipherView {
+}
--- app/src/main/java/ru/codebreakergame/FirstActivity.java
+++ app/src/main/java/ru/codebreakergame/FirstActivity.java
@@ -2,7 +2,6 @@ package ru.codebreakergame;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
@@ -13,14 +12,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class FirstActivity extends AppCompatActivity {
+public class FirstActivity extends AppCompatActivity implements View.OnClickListener {
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
+
+    // шифр цезаря
+    private TextView textView1;
+    private TextView textView2;
+    private TextView textView3;
+    private TextView textView4;
+    private EditText editText0;
+    private EditText editText1;
+    private EditText editText2;
+    private ImageButton buttonUp0;
+    private ImageButton buttonUp1;
+    private ImageButton buttonUp2;
+    private ImageButton buttonDown0;
+    private ImageButton buttonDown1;
+    private ImageButton buttonDown2;
+    private TextView[] lettersArr0 = new TextView[5];
+    private TextView[] lettersArr1 = new TextView[5];
+    private TextView[] lettersArr2 = new TextView[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +84,10 @@ public class FirstActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
+
+        initCaesarCipherTable();
     }
+
 
     private void initToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbaric);
@@ -131,5 +156,146 @@ public class FirstActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.addCategory(Intent.CATEGORY_HOME);
         startActivity(intent);
+    }
+
+    private void initCaesarCipherTable() {
+        // инициация шифра цезаря
+        initEditText();
+        initLetters();
+        initUpAndDownButtons();
+        experimentWithTable();
+    }
+
+    private void initEditText() {
+        editText0 = (EditText) findViewById(R.id.editText10);
+        editText1 = (EditText) findViewById(R.id.editText11);
+        editText2 = (EditText) findViewById(R.id.editText12);
+    }
+
+    private void initLetters() {
+        lettersArr0[0] = (TextView) findViewById(R.id.textView30);
+        lettersArr0[1] = (TextView) findViewById(R.id.textView40);
+        lettersArr0[2] = (TextView) findViewById(R.id.textView50);
+        lettersArr0[3] = (TextView) findViewById(R.id.textView60);
+        lettersArr0[4] = (TextView) findViewById(R.id.textView70);
+
+        for (TextView textView : lettersArr0) {
+        }
+    }
+
+    private void experimentWithTable() {
+
+    }
+
+    private void initUpAndDownButtons() {
+        buttonUp0 = (ImageButton) findViewById(R.id.buttonUp00);
+        buttonUp1 = (ImageButton) findViewById(R.id.buttonUp01);
+        buttonUp2 = (ImageButton) findViewById(R.id.buttonUp02);
+        buttonDown0 = (ImageButton) findViewById(R.id.buttonDown10);
+        buttonDown1 = (ImageButton) findViewById(R.id.buttonDown11);
+        buttonDown2 = (ImageButton) findViewById(R.id.buttonDown12);
+
+        buttonUp0.setOnClickListener(this);
+        buttonUp1.setOnClickListener(this);
+        buttonUp2.setOnClickListener(this);
+        buttonDown0.setOnClickListener(this);
+        buttonDown1.setOnClickListener(this);
+        buttonDown2.setOnClickListener(this);
+    }
+
+
+
+    private void animateLettersDown() {
+
+        Animation аnimationDown_1 = AnimationUtils.loadAnimation(this, R.anim.caesar_cipher_letter_down_1);
+        Animation аnimationDown_2 = AnimationUtils.loadAnimation(this, R.anim.caesar_cipher_letter_down_2);
+        Animation аnimationDown_3 = AnimationUtils.loadAnimation(this, R.anim.caesar_cipher_letter_down_3);
+        Animation аnimationDown_4 = AnimationUtils.loadAnimation(this, R.anim.caesar_cipher_letter_down_4);
+        Animation аnimationDown_100 = AnimationUtils.loadAnimation(this, R.anim.caesar_cipher_letter_down);
+
+
+        textView1.startAnimation(аnimationDown_100);
+        textView2.startAnimation(аnimationDown_100);
+        textView3.startAnimation(аnimationDown_100);
+        textView4.startAnimation(аnimationDown_100);
+ /*       textView1.startAnimation(аnimationDown_1);
+        textView2.startAnimation(аnimationDown_2);
+        textView3.startAnimation(аnimationDown_3);
+        textView4.startAnimation(аnimationDown_4);*/
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.buttonUp00:
+                editTextPlus1(editText0);
+                setLettersUp(lettersArr0);
+                break;
+            case R.id.buttonUp01:
+//                editTextPlus1(editText1);
+//                setLettersUp(lettersArr1);
+                break;
+            case R.id.buttonUp02:
+//                editTextPlus1(editText2);
+//                setLettersUp(lettersArr2);
+                break;
+
+            case R.id.buttonDown10:
+                editTextMinus1(editText0);
+                setLettersDown(lettersArr0);
+                break;
+            case R.id.buttonDown11:
+//                editTextMinus1(editText1);
+//                setLettersDown(lettersArr1);
+                break;
+            case R.id.buttonDown12:
+//                editTextMinus1(editText2);
+//                setLettersDown(lettersArr2);
+                break;
+        }
+    }
+
+    private void editTextPlus1(EditText editText) {
+        String oldStr = editText.getText().toString().trim();
+        int oldInt = Integer.parseInt(oldStr);
+        if (oldInt == 31 || oldInt == -31) {
+            editText.setText("0");
+        } else {
+            editText.setText(Integer.toString(++oldInt));
+        }
+
+    }
+
+    private void editTextMinus1(EditText editText) {
+        String oldStr = editText.getText().toString().trim();
+        int oldInt = Integer.parseInt(oldStr);
+        if (oldInt == 31 || oldInt == -31) {
+            editText.setText("0");
+        } else {
+            editText.setText(Integer.toString(--oldInt));
+        }
+    }
+
+    private void setLettersUp(TextView[] lettersArr) {
+        for (TextView textView : lettersArr) {
+            char oldChar = textView.getText().toString().charAt(0);
+            if (oldChar == 'я') {
+                textView.setText("а");
+            } else {
+                textView.setText(Character.toString(++oldChar));
+            }
+        }
+    }
+
+    private void setLettersDown(TextView[] lettersArr) {
+        for (TextView textView : lettersArr) {
+            char oldChar = textView.getText().toString().charAt(0);
+            if (oldChar == 'а') {
+                textView.setText("я");
+            } else {
+                textView.setText(Character.toString(--oldChar));
+            }
+        }
     }
 }
